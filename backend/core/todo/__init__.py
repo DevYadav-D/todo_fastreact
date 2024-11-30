37f73cd0517233ee8cfc4164a335eb440ccf71f8
DevYadav-D <devyadav5392@gmail.com>
2024-11-30 16:44:15 +0100

    udpated todo backend

---
+++ backend/core/todo/__init__.py
@@ -0,0 +1,1 @@
+#core/todo/__init__.py
---
+++ backend/core/todo/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///:memory:"  
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def init_db():
+    from db.todo.model import Todo
+    Base.metadata.create_all(bind=engine)
+    # Ensure session commit 
+    with engine.begin() as connection: 
+        Base.metadata.create_all(bind=connection)
